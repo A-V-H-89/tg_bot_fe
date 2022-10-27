920def142b6763fed48e3744c9fbb223002679bc
Oleksandr Holenko <golenkoal@gmail.com>
2022-10-28 01:36:57 +0300

    init0

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import './App.css';
 
 
 function App() {
-  useEffect(() => {
-    tg.ready();
-  }, [])
-
-
 
   return (
     <div className="App">
       work properly
-      <button onClick={onClose}>Close</button>
     </div>
   );
 }
