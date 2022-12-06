89364fb17a128ed3d7b1c5b2f96c249e3bfb577e
Oleksandr Holenko <golenkoal@gmail.com>
2022-12-06 02:47:06 +0200

    added form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import './Form.css';
+
+function Form() {
+    const [country, setCountry] = useState('')
+    const [street, setStreet] = useState('')
+    const [subject, setSubject] = useState('phisical')
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Sent the data'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [country, street])
+
+    const onChangeCountry = e => setCountry(e.target.value)
+    const onChangeStreet = e => setStreet(e.target.value)
+    const onChangeSubject = e => setSubject(e.target.value)
+
+  return (
+    <div className='form'>
+      <h3>Please fill the form</h3>
+      <input type='text' className='input' placeholder='Street'value={street} onChange={onChangeStreet}/>
+      <input type='text' className='input' placeholder='Country' value={country} onChange={onChangeCountry}/>
+      <select className='select' value={subject} onChange={onChangeSubject}>
+        <option value={'phisical'}>Phisical worker</option>
+        <option value={'mental'}>Mental worker</option>
+      </select>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductList.css';
+
+function ProductList() {
+  return (
+    <div>
+      ProductList
+    </div>
+  )
+}
+
+export default ProductList
