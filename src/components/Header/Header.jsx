7901b94e6aca2323697d86c27ad0455bfe0b83d9
Oleksandr Holenko <golenkoal@gmail.com>
2022-12-06 02:12:00 +0200

    added check of existing field

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,11 @@ import Button from '../Button/Button';
 
 function Header() {
   const { user, onClose } = useTelegram();
-  const { username } = window.Telegram.WebApp.initDataUnsafe.user
   
-
   return (
     <div className='header'>
       <Button onClick={onClose}>Close</Button>
-      <span className='userName'>{username}</span>
+      <span className='userName'>{user?.username}</span>
     </div>
   );
 }
