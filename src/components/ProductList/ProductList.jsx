99cae51acc37d2d34ac9bbdf924e2e5853b5b8cd
Oleksandr Holenko <golenkoal@gmail.com>
2023-04-25 20:45:43 +0300

    last changes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import ProductItem from '../ProductItem/ProductItem';
@@ -12,17 +14,40 @@ const products = [
 ];
 
 const getTotalPrice = (items) => {
-    return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 function ProductList() {
   const [addedItems, setAddedItems] = useState([]);
-  const {tg} = useTelegram()
+  const { tg, queryId } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+        products: addedItems,
+        totalPrice: getTotalPrice(addedItems),
+        queryId
+    };
+
+    fetch('http://localhost:8000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+  }, []);
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData]);
 
   const onAdd = (product) => {
-    const  alreadyAdded = addedItems.find((item) => item.id === product.id);
+    const alreadyAdded = addedItems.find((item) => item.id === product.id);
     let newItems = [];
 
     if (alreadyAdded) {
@@ -31,15 +56,15 @@ function ProductList() {
       newItems = [...addedItems, product];
     }
 
-    setAddedItems(newItems)
+    setAddedItems(newItems);
 
-    if(newItems.length === 0) {
-        tg.MainButton.hide()
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
     } else {
-        tg.MainButton.show()
-        tg.MainButton.setParams({
-            text: `Buy ${getTotalPrice(newItems)}`
-        })
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Buy ${getTotalPrice(newItems)}`,
+      });
     }
   };
 
